5f5f6866a2d1a1a47d25be36e77ceb782b37aa0d
ShofiSo <kelly.rabanales@gmail.com>
2026-05-06 21:50:35 -0600

    Cuarto Commit
    Se añadió a server.js, db para la conexión, y que podamos validar si en dado caso hay un error en la base de datos.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 
 const app = express();
 app.use(cors());
@@ -7,6 +8,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("API ControlGastos funcionando");
+});
+
+app.get("/test-db", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error en la base de datos");
+    }
 });
 
 app.listen(3000, () => {

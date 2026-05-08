cadaa8a1a05e931bd54271c755035d670e8affda
ShofiSo <kelly.rabanales@gmail.com>
2026-05-07 21:23:18 -0600

    Octavo Commit
    Se acrualiza para poder ver cambios en la DB

--- backend/db.js
+++ backend/db.js
@@ -4,8 +4,16 @@ const pool = new Pool({
     user: "postgres",
     host: "localhost",
     database: "controlgastos",
-    password: "1234",
+    password: "1234", 
     port: 5432,
+});
+
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error("Error al conectar a la BD", err.stack);
+    }
+    console.log("Conectado a PostgreSQL");
+    release();
 });
 
 module.exports = pool;
